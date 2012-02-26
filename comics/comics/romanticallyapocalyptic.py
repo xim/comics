fdea7223f33b8bb510fdf17976cb52eb63b5b926
xim <xim-git@akuma.no>
2012-02-26 16:46:39 +0100

    Use html2text to parse rom.ac diary entry thing

--- comics/comics/romanticallyapocalyptic.py
+++ comics/comics/romanticallyapocalyptic.py
@@ -1,6 +1,14 @@
 from comics.aggregator.crawler import CrawlerBase, CrawlerImage
 from comics.meta.base import MetaBase
 
+from lxml import html
+import html2text
+
+html2text.BODY_WIDTH = 0
+html2text.SKIP_INTERNAL_LINKS = True
+html2text.UNICODE_SNOB = True
+
+
 class Meta(MetaBase):
     name = 'Romantically Apocalyptic'
     language = 'en'
@@ -15,6 +23,11 @@ class Crawler(CrawlerBase):
     def crawl(self, pub_date):
         page = self.parse_page('http://www.romanticallyapocalyptic.com/')
         urls = page.src('img[src*="/art/"]', allow_multiple=True)
+        title = None
+        text = html.tostring(page._select('div.stand_high')).strip()
+        if text:
+            text = html2text.html2text(text).strip()
+
         for url in urls:
             if 'thumb' not in url:
-                return CrawlerImage(url)
+                return CrawlerImage(url, title, text)
